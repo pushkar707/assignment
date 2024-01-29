1a3c9fd9aed802b904a152aad00ffa803edc9539
Pushkar <bansalpushkar100@gmail.com>
2024-01-29 17:33:52 +0530

    responsiveness correction

--- client/src/app/blog/read/[blogId]/page.tsx
+++ client/src/app/blog/read/[blogId]/page.tsx
@@ -24,15 +24,15 @@ const Page = ({params}:any) => {
   if(blogDetails.title){
     return (
       <>
-        <div className='bg-[#1188F0] py-16 w-full flex justify-center items-center flex-col text-white mt-5 px-5' >
-          <p className='text-3xl mb-3 font-medium'>{blogDetails.title}</p>
+        <div className='bg-[#1188F0] py-16 w-full flex justify-center items-center flex-col text-white mt-5 px-1 lg:px-5' >
+          <p className='text-2xl md:text-3xl mb-4 font-medium text-center'>{blogDetails.title}</p>
           <p className='text-lg mb-2 font-light'>{blogDetails.tags.join(" ")}</p>
           <p className='font-light'>Published On: {blogDetails.createdAt.slice(0,10)}</p>
         </div>
 
 
         <div className='p-10'>
-          <img src={`https://${process.env.NEXT_PUBLIC_AWS_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_AWS_BUCKET_REGION}.amazonaws.com/`+blogDetails.imageKey} className='block mb-8 mx-auto w-[70&] h-[400px]' />
+          <img src={`https://${process.env.NEXT_PUBLIC_AWS_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_AWS_BUCKET_REGION}.amazonaws.com/`+blogDetails.imageKey} className='block mb-8 mx-auto' />
           <pre className='whitespace-pre-line'>{blogDetails.desc}</pre>
         </div>
       
--- client/src/app/components/PostCard.tsx
+++ client/src/app/components/PostCard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PostCard = ({_id,imageKey,title,tags,createdAt}:any) => {
   return (
     <div>
-        <img src={`https://${process.env.NEXT_PUBLIC_AWS_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_AWS_BUCKET_REGION}.amazonaws.com/`+imageKey} className="w-full h-[180px]" />
+        <img src={`https://${process.env.NEXT_PUBLIC_AWS_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_AWS_BUCKET_REGION}.amazonaws.com/`+imageKey} className="w-full max-h-[330px] md:h-[180px]" />
         <div className="bg-[#0375FF14] p-4">
         <p className="mb-3 font-medium">{title.slice(0,75)}{title.length > 75 ? '....' : ''}</p>
         <p className="mb-2 flex flex-wrap"><span className='me-2'>Tags:</span>{tags.map((tag:string,index:number) => {
